2cec4755075da7fa5d32629144f4481764ebbdfc
johnallec <clegnn95p30c352m@studenti.unical.it>
2021-11-16 23:23:12 +0100

    editing ServerOggetti.....

--- src/main/java/com/johnny/strutturali/ServerOggetti_PR/client/Main.java
+++ src/main/java/com/johnny/strutturali/ServerOggetti_PR/client/Main.java
@@ -1,19 +1,63 @@
 package com.johnny.strutturali.ServerOggetti_PR.client;
 
-import com.johnny.strutturali.ServerOggetti_PR.proxy.OggettoProxy;
-import com.johnny.strutturali.ServerOggetti_PR.proxy.Testo;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.awt.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		OggettoProxy o = new OggettoProxy();
-		Testo t1 = o.costruisciTesto("testo1", "Questo è il testo numero 1");
-		Testo t2 = o.costruisciTesto("testo2", "Questo è il testo numero 2");
-		Testo t3 = o.costruisciTesto("testo3", "Questo è il testo numero 3");
-		Testo t4 = o.costruisciTesto("testo1", "Questo è il testo numero 4");
-		System.out.println(t1.getNome() + " -> " + t1.getTesto());
-		System.out.println(t2.getNome() + " -> " + t2.getTesto());
-		System.out.println(t3.getNome() + " -> " + t3.getTesto());
-		System.out.println(t4.getNome() + " -> " + t4.getTesto());
+	public static void main(String[] args) throws IOException {
+		JFrame frame = new JFrame();
+		frame.setSize(800,800);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel(new BorderLayout());
+		frame.add(panel);
+		JFileChooser jfc = new JFileChooser();
+		jfc.setAcceptAllFileFilterUsed(false);
+		jfc.addChoosableFileFilter(new FileNameExtensionFilter("Images","jpg","jpeg","png"));
+		int res = jfc.showOpenDialog(panel);
+		if(res == JFileChooser.APPROVE_OPTION){
+			File file = jfc.getSelectedFile();
+			JLabel label = new JLabel();
+			label.setHorizontalAlignment(JLabel.CENTER);
+			label.setSize(new Dimension(100,100));
+			label.setVisible(true);
+			Image img = ImageIO.read(file);
+			img = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
+			label.setIcon(new ImageIcon(img));
+			panel.add(label, BorderLayout.CENTER);
+			JPanel panel2 = new JPanel();
+			BoxLayout bl = new BoxLayout(panel2, BoxLayout.LINE_AXIS);
+			panel2.setLayout(bl);
+			panel.add(panel2, BorderLayout.PAGE_END);
+			panel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+			JButton ahead = new JButton("Ahead");
+			ahead.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+
+				}
+			});
+			JButton back = new JButton("Back");
+			back.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					
+				}
+			});
+			ahead.setHorizontalAlignment(JButton.CENTER);
+			back.setHorizontalAlignment(JButton.CENTER);
+			ahead.setMargin(new Insets(10, 10, 10, 10));
+			back.setMargin(new Insets(10, 10, 10, 10));
+			panel2.add(ahead);
+			panel2.add(back);
+		}
+		
 	}
 }
